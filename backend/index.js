a9b0267c4deba582cd8f76a5049c171f1cd11d45
‘slee’ <leeseungho453@gmail.com>
2024-03-11 21:37:11 +0900

    project update

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+// creating api request using api server
+import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import morgan from 'morgan' 
+import bodyParser from "body-parser"; 
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT || 9000
+
+
+// DB connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log("DB connected"))
+.catch((err) => console.log(err));
+
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: "5mb"}));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+// for authentication
+app.use(cookieParser());
+// to make request to the backend
+// to allow application to use backend api to access to the server
+app.use(cors())
+app.use(express.json())
+
+
+
+// this message will appear on the console if the backend server is connected
+app.listen(port, () => {
+    console.log(`Backend Connected to Port ${port}`)
+});
