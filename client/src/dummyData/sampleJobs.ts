826f92b79092e2d502b74cfe1a8c29ded68f9f09
‘slee’ <leeseungho453@gmail.com>
2024-04-30 13:40:12 +0900

    dummy data & Home.tsx updated to the same version as frontend

--- client/src/dummyData/sampleJobs.ts
+++ client/src/dummyData/sampleJobs.ts
@@ -1,23 +1,56 @@
-export const exampleJob1 = {
-    jobTitle: "Software Engineer",
-    description: "Develop and maintain web applications using React, TypeScript, and Node.js.",
-    category: "IT/Software",
-    location: "San Francisco, CA",
-    id: "1"
-};
+import type { JobData, FilterData } from "../types/Job";
 
-export const exampleJob2 = {
-    jobTitle: "Frontend Engineer",
-    description: "Develop and maintain web applications using React, TypeScript, and Node.js.",
-    category: "IT/Software",
-    location: "San Francisco, CA",
-    id: "2"
-};
+export const exampleJob: JobData = {
+    data: [
+        {
+            jobTitle: "Software Engineer",
+            description: "Develop and maintain web applications using React, TypeScript, and Node.js.",
+            category: "IT/Software",
+            location: "San Francisco, CA",
+            id: "1"
+        },
+        {
+            jobTitle: "Frontend Engineer",
+            description: "Develop and maintain web applications using React, TypeScript, and Node.js.",
+            category: "IT/Software",
+            location: "San Francisco, CA",
+            id: "2"
+        },
+        {
+            jobTitle: "Backend Engineer",
+            description: "Develop and maintain web applications using React, TypeScript, and Node.js.",
+            category: "IT/Software",
+            location: "San Francisco, CA",
+            id: "3"
+        }
+    ]
+}
 
-export const exampleJob3 = {
-    jobTitle: "Backend Engineer",
-    description: "Develop and maintain web applications using React, TypeScript, and Node.js.",
-    category: "IT/Software",
-    location: "San Francisco, CA",
-    id: "3"
-};
+export const filterData: FilterData = {
+    data: [
+        {
+            title: "Company",
+            options: [
+                { value: "", name: "All" },
+                { value: "corporate", name: "Corporate" },
+                { value: "startups", name: "StartUps" }
+            ]
+        },
+        {
+            title: "Job Categories",
+            options: [
+                { value: "", name: "All" },
+                { value: "IT/Software", name: "IT / Software" },
+                { value: "Marketing", name: "Marketing" }
+            ]
+        },
+        {
+            title: "Location",
+            options: [
+                { value: "", name: "All" },
+                { value: "seoul", name: "Seoul" },
+                { value: "busan", name: "Busan" },
+            ]
+        }
+    ]
+}
