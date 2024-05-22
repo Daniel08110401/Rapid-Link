5e7bed0bf904f0d89738c36472aff44901218160
‘slee’ <leeseungho453@gmail.com>
2024-05-22 19:16:40 +0900

    job page for naver is cleared

--- client/src/component/UserMenu.tsx
+++ client/src/component/UserMenu.tsx
@@ -1,95 +1,3 @@
-// import * as React from 'react';
-// import { useTheme } from '@mui/material/styles';
-// import { useNavigate, Link as RouterLink } from 'react-router-dom';
-// import { Avatar } from '@mui/material';
-// import { useSetRecoilState, useResetRecoilState } from 'recoil';
-// import { userLogInState } from '../recoil/atoms/userState';
-// import { UserSignOut } from '../recoil/selectors/userSignOut'; // Import the userSignOut selector
-// import {
-//   UserMenuContainer,
-//   UserMenuIconButton,
-//   UserMenuTooltip,
-//   UserMenuStyledMenu,
-//   UserMenuStyledMenuItem,
-//   UserMenuTypography,
-// } from '../style/UserMenuStyles';
-
-// const UserMenu = () => {
-//     const signOut = UserSignOut();
-//     const navigate = useNavigate();
-//     const theme = useTheme();
-//     const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-//     const handleOpenUserMenu = (event:any) => {
-//         setAnchorElUser(event.currentTarget);
-//     };
-
-//     const handleCloseUserMenu = () => {
-//         setAnchorElUser(null);
-//     };
-
-//     const logOutUser = async () => {
-//         await signOut(); // Use the sign out hook
-//         navigate('/');
-//         window.location.reload();
-//     };
-//     return (
-//         <UserMenuContainer>
-//             <UserMenuTooltip title="Open settings">
-//                 <UserMenuIconButton onClick={handleOpenUserMenu}>
-//                     <Avatar alt="User Avatar" src="" />
-//                 </UserMenuIconButton>
-//             </UserMenuTooltip>
-//             <UserMenuStyledMenu
-//                 id="menu-appbar"
-//                 anchorEl={anchorElUser}
-//                 anchorOrigin={{
-//                     vertical: 'top',
-//                     horizontal: 'right',
-//                 }}
-//                 keepMounted
-//                 transformOrigin={{
-//                     vertical: 'top',
-//                     horizontal: 'right',
-//                 }}
-//                 open={Boolean(anchorElUser)}
-//                 onClose={handleCloseUserMenu}
-//             >
-//                 <UserMenuStyledMenuItem onClick={handleCloseUserMenu}>
-//                     <UserMenuTypography>
-//                         <RouterLink
-//                             to="/admin/dashboard"
-//                             style={{ textDecoration: 'none', color: theme.palette.primary.main }}
-//                         >
-//                             Profile
-//                         </RouterLink>
-//                     </UserMenuTypography>
-//                 </UserMenuStyledMenuItem>
-
-//                 <UserMenuStyledMenuItem onClick={handleCloseUserMenu}>
-//                     <UserMenuTypography>
-//                         <RouterLink
-//                             to="/login"
-//                             style={{ textDecoration: 'none', color: theme.palette.primary.main }}
-//                         >
-//                             Sign In
-//                         </RouterLink>
-//                     </UserMenuTypography>
-//                 </UserMenuStyledMenuItem>
-            
-//                 {/* Assuming user state is determined to decide if showing logout */}
-//                 <UserMenuStyledMenuItem onClick={logOutUser}>
-//                     <UserMenuTypography style={{ textDecoration: 'none', color: theme.palette.primary.main }}>
-//                         Sign Out
-//                     </UserMenuTypography>
-//                 </UserMenuStyledMenuItem>
-//             </UserMenuStyledMenu>
-//         </UserMenuContainer>
-//     );
-// };
-
-// export default UserMenu;
-
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import { useNavigate, Link } from 'react-router-dom';
@@ -127,7 +35,7 @@ const UserMenu = () => {
 
     const handleProfileClick = () => {
         if (user.isAuthenticated) {
-            navigate('/profile');
+            navigate('/user/profile');
         } else {
             navigate('/login');
         }
